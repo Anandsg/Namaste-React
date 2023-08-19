956999fb8ab43eb5255b7ceb77db933e15e45b85
Anandagadugin <gaduginanand984@gmail.com>
2023-08-20 00:44:21 +0530

    Episode 01 Inception assignment solved

--- App.js
+++
@@ -1,8 +0,0 @@
-const heading = React.createElement(
-    "h1",
-    { id: "heading" },
-    "Hellow worlds from react!"
-);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(heading);
